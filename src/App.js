e052688f038219fcea416b67546230ba691396f2
Lorkerty <lorkerty@gmail.com>
2019-12-09 00:54:26 +0200

    NA SUKA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+class App extends Component{
+  render()
+  {
+    return (
+    <div className = "karto4ki">
+      <div className = "karta">
+        <img align = "center" src = "http://your-italy.ru/wp-content/uploads/2015/07/Domeniko-Girlandayo-avtoportret.jpg"/>
+        <h3>Mikilanjela</h3>
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "karta">
+        <img align = "center" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuOkqIBUOHzGcLgH3wYuJDrD8b5x_tACctUzkjaJsoZWWqrJCI"/>
+        <h3>One Gog</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTg4Lx64F3FRiSlCufU9xre2HDqWel-7_11i-LEiKm3JydvHGSg"/>
+        <h3>Two Gog</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://stuki-druki.com/biofoto/Oleg-Maiami-01.jpg"/>
+        <h3>Олег</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "http://memesmix.net/media/created/8gew04.jpg"/>
+        <h3>Следи за собой</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://newizv.ru/attachments/0d6259d878c5dba061e1e09b7c3f733d2a947011/store/fill/780/440/12dcb1ba3271f2ef54f6da75c1da51d7e9e04c00b74612f6503602877930/%D1%86%D0%BE%D0%B9.jpg"/>
+        <h3>Жив</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://newizv.ru/attachments/0d6259d878c5dba061e1e09b7c3f733d2a947011/store/fill/780/440/12dcb1ba3271f2ef54f6da75c1da51d7e9e04c00b74612f6503602877930/%D1%86%D0%BE%D0%B9.jpg"/>
+        <h3>Жив</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://newizv.ru/attachments/0d6259d878c5dba061e1e09b7c3f733d2a947011/store/fill/780/440/12dcb1ba3271f2ef54f6da75c1da51d7e9e04c00b74612f6503602877930/%D1%86%D0%BE%D0%B9.jpg"/>
+        <h3>Жив</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://newizv.ru/attachments/0d6259d878c5dba061e1e09b7c3f733d2a947011/store/fill/780/440/12dcb1ba3271f2ef54f6da75c1da51d7e9e04c00b74612f6503602877930/%D1%86%D0%BE%D0%B9.jpg"/>
+        <h3>Жив</h3>
+      </div>
+
+      <div className = "karta">
+        <img align = "center" src = "https://newizv.ru/attachments/0d6259d878c5dba061e1e09b7c3f733d2a947011/store/fill/780/440/12dcb1ba3271f2ef54f6da75c1da51d7e9e04c00b74612f6503602877930/%D1%86%D0%BE%D0%B9.jpg"/>
+        <h3>Жив</h3>
+      </div>
+
+      
     </div>
-  );
+    )
+     
+  }
 }
-
 export default App;
